47f0dc13399499cfcf7e7a2c710f81445d273cb0
KrisTan2326 <tanwarkrishnapalsingh@gmail.com>
2024-10-07 13:57:22 +0530

    Krishna Pal Singh Tanwar

--- program1.py
+++ program1.py
@@ -1,20 +1,17 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for char in s:
+            if not stack:
+                stack.append(char)
+            elif char in "({[":
+                stack.append(char)
+            elif (char == ')' and stack[-1] == '(') or \
+                 (char == '}' and stack[-1] == '{') or \
+                 (char == ']' and stack[-1] == '['):
+                stack.pop()
+            else:
+                stack.append(char)
 
-
-
-
-
-
-
-    
-
-
-
-  
-
+        return not stack
--- program2.py
+++ program2.py
@@ -1,10 +1,23 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        if not s:  # Check if the input string is empty
+            return 0
+        
+        res = 0
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
+        for a, b in zip(s, s[1:]):
+            if roman[a] < roman[b]:
+                res -= roman[a]
+            else:
+                res += roman[a]
 
-
+        return res + roman[s[-1]]  
